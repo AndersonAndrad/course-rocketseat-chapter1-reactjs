b1fdd3ca0bee7cd4d774c6de288dd93236ae03a4
AndersonAndrad <31743641+AndersonAndrad@users.noreply.github.com>
2021-07-21 23:32:08 -0300

    config - adding the dinamic index in webpack config with htmlwebpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
-const path = require('path')
+const path = require( 'path' )
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' )
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.jsx'),
+  entry: path.resolve( __dirname, 'src/index.jsx' ),
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve( __dirname, 'dist' ),
     filename: 'bundle.js',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  plugins: [
+    new HtmlWebpackPlugin( { template: path.resolve( __dirname, 'public', 'index.html' ) } ),
+  ],
   module: {
     rules: [
       {
